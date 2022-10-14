cf9ac75a08918b97c8bc87d93ffc59d99e7009e5
Saoumaa <104036386+Saoumaa@users.noreply.github.com>
2022-10-14 21:11:08 +0100

    Add files via upload
    2

---
+++ routes/web.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+  
+  /* la route des produits*/
+Route::get('/boutique','ProductController@index')->name('products.index');
+
+Route::get('/boutique/{slug}','ProductController@show')->name('products.show');
+
+Route::get('/search', 'ProductController@search')->name('products.search');
+
+
+Route::group(['middleware' => ['auth']], function () {
+
+
+/* la route vert le panier */
+
+Route::get('/panier','CartController@index')->name('cart.index');
+
+Route::post('/panier/ajouter','CartController@store')->name('cart.store');
+
+Route::patch('/panier/{rowId}', 'CartController@update')->name('cart.update');
+
+Route::delete('/panier/{rowId}','CartController@destroy')->name('cart.destroy');
+
+ 
+});
+
+
+Route::group(['middleware' => ['auth']], function () {
+
+/* route vers checkout*/
+
+Route::get('/paiement','CheckoutController@index')->name('checkout.index');
+
+Route::post('/paiement','CheckoutController@store')->name('checkout.store');
+
+Route::get('/merci','CheckoutController@thankyou')->name('checkout.thankyou');
+
+});
+
+
+
+
+Auth::routes();
+
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
